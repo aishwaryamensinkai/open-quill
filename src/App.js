3e2983aa11abe4cb5cd632ef3dce46a07dd7c453
Aishwarya Mensinkai <aishwaryamensinkai@gmail.com>
2024-07-03 16:23:59 -0400

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "swiper/swiper.min.css";
 import "./App.scss";
 
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
@@ -10,7 +10,7 @@ import Routes from "./config/Routes";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Route
         render={(props) => (
           <>
@@ -20,7 +20,7 @@ function App() {
           </>
         )}
       />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
