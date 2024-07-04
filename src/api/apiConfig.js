0ac1e4990aa374b8f676c8748962bd9b3ab446bc
Aishwarya Girish Mensinkai <48703781+aishwaryamensinkai@users.noreply.github.com>
2024-07-04 13:59:51 -0400

    Update apiConfig.js with api

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,7 +1,6 @@
 const apiConfig = {
   baseUrl: "https://api.themoviedb.org/3/",
-  // apiKey: "your-api-key", // Replace with your actual API key
-  apiKey: "b7f92695a2205e8be97222fad88e8773",
+  apiKey: "your-api-key", // Replace with your actual API key
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
